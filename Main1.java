330d30fcf62478ecd0a703223004f7328bb5cea1
Nikita-Aksenov <lulka322@gmail.com>
2024-06-24 16:32:25 +0300

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,11 @@
+
+public class Main1 {
+    public static void main(String[] args) {
+        Employee[] person = new Employee[5];
+        person[0] = new Employee("Nikita"," Junior"," pepsi123@gmail.com ", 255651122, 30000,28);
+        person[1] = new Employee("Angel"," Senior"," cola228@gmail.com " , 297852511, 55000,35);
+        person[2] = new Employee("Dmitriy", " Junior"," fanta123@gmail.com " , 257651182, 45000,31);
+        person[3] = new Employee("Anna" , " Junior", " grid221@gmail.com ", 298003351, 25000,28);
+        person[4] = new Employee("Kate" , " Junior", " dobra123@gmail.com ", 251235691, 31000,29);
+    }
+}
